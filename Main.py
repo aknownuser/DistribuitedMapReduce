c96f4979b882d706eebf6ef31c484aefddb44297
Oussama El Azizi <oussamaelazizi.opt@gmail.com>
2018-03-21 19:49:51 +0100

    Changes in sequential to do speedup

--- Main.py
+++ Main.py
@@ -1,16 +1,22 @@
 import time
 import functools
 from collections import Counter
+import unicodedata, sys
+
+def remove_accent_mark(s):
+    s = s.decode('utf-8')
+    result = ''.join((c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn'))
+    return result.encode('utf-8')
 
 def word_count(f):
     word_dict = Counter()
-    punc = ',.!?-*&^%$#@'
+    punc = ',.!?-*&^%$#@[]'
     #  with will automatically close your file
     with open(f) as in_file:
         # iterate over each line
         for line in in_file:
             # pass each stripped word from the line to the Counter dict
-            mapped_words = map(lambda x :x.rstrip(punc).lower(), line.split())
+            mapped_words =filter(lambda x: x != '', map(lambda x: remove_accent_mark(x.strip(punc).lower()), line.split()))
             word_dict.update(mapped_words)
     return word_dict
 
@@ -19,21 +25,24 @@ def counting_word(file_name):
     punc = ',.!?-*&^%$#@'
     with open(file_name) as file:
         for line in file:
-            line = map(lambda x: x.rstrip(punc).lower(),line.split())
-            count = count + len(filter(lambda x: x != '' and x != '\n',line))
+            count = count + len(filter(lambda x: x != '', map(lambda x: remove_accent_mark(x.strip(punc).lower()), line.split())))
     return count
 
 if __name__ == "__main__":
-    #print "----------COUNT WORDS-----------"
+
+    if len(sys.argv) != 2:
+        print "Not enough paramaters: You need to put the files name."
+
+    print "----------COUNT WORDS-----------"
     #start_time = time.time()
     #print "Sherlok Holmes: "+str(counting_word("big.txt"))
     #elapsed_time = time.time() - start_time
     #print "Elapsed time: "+str(elapsed_time)
 
-    #start_time = time.time()
-    #print "\nEl Quijote: " + str(counting_word("pg2000.txt"))
-    #elapsed_time = time.time() - start_time
-    #print "Elapsed time: " + str(elapsed_time)
+    start_time = time.time()
+    print "\nEl Quijote: " + str(counting_word(sys.argv[1]))
+    elapsed_time = time.time() - start_time
+    print "Elapsed time: " + str(elapsed_time)
     #PG10.txt
     #start_time = time.time()
     #print "\nThe bible: "+str(counting_word("pg10.txt"))
@@ -48,7 +57,7 @@ if __name__ == "__main__":
     #print "Elapsed time: " + str(elapsed_time)
 
     start_time = time.time()
-    print "\nEl Quijote: \n" + str(word_count("pg2000.txt"))
+    print "\nEl Quijote: \n" + str(word_count(sys.argv[1]))
     elapsed_time = time.time() - start_time
     print "Elapsed time: " + str(elapsed_time)
     # PG10.txt
