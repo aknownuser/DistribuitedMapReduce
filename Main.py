fdaf7d0564fa6210f559de5bfa0f8b3c8075549d
Oussama El Azizi <oussamaelazizi.opt@gmail.com>
2018-04-09 21:05:33 +0200

    Codacy

--- Main.py
+++ Main.py
@@ -1,11 +1,8 @@
 import time
 from collections import Counter
-import unicodedata, sys, os
+import sys
+import os
 
-def remove_accent_mark(s):
-    s = s.decode('utf-8')
-    result = ''.join((c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn'))
-    return result.encode('utf-8')
 
 def word_count(f):
     word_dict = Counter()
@@ -15,7 +12,8 @@ def word_count(f):
         # iterate over each line
         for line in in_file:
             # pass each stripped word from the line to the Counter dict
-            mapped_words =filter(lambda x: x != '', map(lambda x: x.strip(punc).lower(), line.split()))
+            #mapped_words =filter(lambda x: x != '', map(lambda x: x.strip(punc).lower(), line.split()))
+            mapped_words = [val for val in [x_val.strip(punc).lower() for x_val in line.split()] if val != '']
             word_dict.update(mapped_words)
     return word_dict
 
@@ -24,11 +22,12 @@ def counting_word(file_name):
     punc = ',.!?-*&^%$#@[]()'
     with open(file_name) as file:
         for line in file:
-            count = count + len(filter(lambda x: x != '', map(lambda x: x.strip(punc).lower(), line.split())))
+            #count = count + len(filter(lambda x: x != '', map(lambda x: x.strip(punc).lower(), line.split())))
+            count = count + len([val for val in [x_val.strip(punc).lower() for x_val in line.split()] if val != ''])
     return count
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     if len(sys.argv) <= 1:
         print "Not enough paramaters: You need to put the files name."
     i = 0
@@ -40,13 +39,13 @@ if __name__ == "__main__":
         print "----------COUNT WORDS-----------"
         print str(x) + ": " + " Size: "+str(statinfo.st_size)
         start_time = time.time()
-        counting_word(x)
+        print counting_word(x)
         elapsed_time = time.time() - start_time
         print "Elapsed time: " + str(elapsed_time)
 
         print "----------WORD COUNT----------"
         print str(x) + ": " + " Size: " + str(statinfo.st_size)
         start_time = time.time()
-        word_count(x)
+        print word_count(x)
         elapsed_time = time.time() - start_time
         print "Elapsed time: " + str(elapsed_time)
